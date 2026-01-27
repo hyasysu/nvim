474cfd5acb0263283d16d32c4033e0bf5c948bb0
hya <hyasysujx@163.com>
2026-01-27 12:21:06 +0800

    Refactor bufferline configuration; replace dependency with direct function call for closing buffers

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,7 +3,7 @@ local plain = not require("core.options").nerd_fonts
 return {
     "akinsho/bufferline.nvim",
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "famiu/bufdelete.nvim" },
+    -- dependencies = { "famiu/bufdelete.nvim" },
     opts = {
         options = {
             buffer_close_icon = plain and 'x' or nil,  -- Close icon for each buffer
@@ -58,16 +58,23 @@ return {
         },
     },
     keys = {
-        { "]b",         "<cmd>BufferLineCycleNext<cr>",            mode = { "n", "v" }, desc = "Switch to next buffer" },
-        { "[b",         "<cmd>BufferLineCyclePrev<cr>",            mode = { "n", "v" }, desc = "Switch to previous buffer" },
-        { "<b",         "<cmd>BufferLineMovePrev<cr>",             mode = { "n", "v" }, desc = "Move buffer to previous position" },
-        { ">b",         "<cmd>BufferLineMoveNext<cr>",             mode = { "n", "v" }, desc = "Move buffer to next position" },
+        { "]b",         "<cmd>BufferLineCycleNext<cr>", mode = { "n", "v" }, desc = "Switch to next buffer" },
+        { "[b",         "<cmd>BufferLineCyclePrev<cr>", mode = { "n", "v" }, desc = "Switch to previous buffer" },
+        { "<b",         "<cmd>BufferLineMovePrev<cr>",  mode = { "n", "v" }, desc = "Move buffer to previous position" },
+        { ">b",         "<cmd>BufferLineMoveNext<cr>",  mode = { "n", "v" }, desc = "Move buffer to next position" },
 
-        { "<leader>bp", "<cmd>BufferLineTogglePin<cr>",            mode = { "n", "v" }, desc = "Toggle buffer pin" },
-        { "<leader>bc", "<cmd>bdelete<cr>",                        mode = { "n" },      desc = "Close buffer" },
-        { "<leader>bd", "<cmd>BufferLineCloseOthers<cr>",          mode = { "n" },      desc = "Close other buffers" },
-        { "<leader>bf", "<cmd>BufferLinePick<cr>",                 mode = { "n" },      desc = "Search and jump to buffer" },
+        { "<leader>bp", "<cmd>BufferLineTogglePin<cr>", mode = { "n", "v" }, desc = "Toggle buffer pin" },
+        {
+            "<leader>bc",
+            function()
+                Snacks.bufdelete.delete()
+            end,
+            mode = { "n" },
+            desc = "Close buffer"
+        },
+        { "<leader>bd", "<cmd>BufferLineCloseOthers<cr>",          mode = { "n" }, desc = "Close other buffers" },
+        { "<leader>bf", "<cmd>BufferLinePick<cr>",                 mode = { "n" }, desc = "Search and jump to buffer" },
 
-        { "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", mode = { "n" },      desc = "Close ungrouped buffers", },
+        { "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", mode = { "n" }, desc = "Close ungrouped buffers", },
     },
 }
