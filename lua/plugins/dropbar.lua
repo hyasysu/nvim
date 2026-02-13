12fac9fdc1f6a7631237cdf633cad88dd07560a9
ian.huang <ian.huang@metaverse-hk.cloud>
2026-02-13 16:00:14 +0800

    dropbar keymap

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -9,6 +9,7 @@ return {
         config = function()
             local dropbar_api = require('dropbar.api')
             vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'DropBar - Pick symbols in winbar' })
+            vim.keymap.set('n', '<leader>ld', dropbar_api.pick, { desc = 'DropBar - Pick symbols in winbar' })
             vim.keymap.set('n', '[;', dropbar_api.goto_context_start,
                 { desc = 'DropBar - Go to start of current context' })
             vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'DropBar - Select next context' })
